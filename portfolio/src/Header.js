2b3b7faabb84b93587015b6eb2e38074ae85d106
Nina Hawari <nina.hawari@gmail.com>
2023-04-11 23:48:05 +0300

    added noise

---
+++ portfolio/src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import backgroundImage from '/Users/netizency/Desktop/Bootcamp/portfolio/portfolio/src/5502258.png';
+import './App.css';
+
+function Header() {
+
+  return (
+    <div>
+
+      <div  ></div>
+      <div className='fragment' style={{ backgroundImage: `url(${backgroundImage})` }}>Header</div>
+    </div>
+  )
+}
+
+export default Header
